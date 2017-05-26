b386a34beb14805509b3d90bd0f23b7180c85cd5
Wengao Ye <elleryqueenhomels@users.noreply.github.com>
2017-05-27 00:49:52 +0800

    A Test Demo

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
 	save_path = 'models/starry.ckpt-done'
 
 	if is_training:
-		content_targets = list_images('./images/content')
+		content_targets = list_images('images/content')
 		print('\ncontent_targets:\n', content_targets, '\n')
 
 		train(content_targets, style_target, content_weight, style_weight, tv_weight, save_path=save_path, batch_size=2, debug=True)
@@ -26,10 +26,10 @@ if __name__ == '__main__':
 		model_path = save_path
 
 		if condition == 1:
-			content_targets = list_images('./images/content')
+			content_targets = list_images('images/content')
 			generated_images = generate(content_targets, model_path, resize_height=300, resize_width=300, postfix='-starry')
 		elif condition == 2:
-			content_targets = list_images('./images/content')
+			content_targets = list_images('images/content')
 			generated_images = generate(content_targets, model_path, postfix='-starry')
 		elif condition == 3:
 			content_targets = ['images/content/lena.png', 'images/content/lena2.png', 'images/content/lena3.png', 'images/content/lena4.png']
