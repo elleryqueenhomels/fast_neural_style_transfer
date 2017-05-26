a9caa58c6a1a456f18c25c29c16c3da287448f9a
Wengao Ye <elleryqueenhomels@users.noreply.github.com>
2017-05-26 23:06:45 +0800

    Utility

--- utils.py
+++ utils.py
@@ -2,45 +2,69 @@
 
 import tensorflow as tf
 
-
-def get_image(path, height=None, width=None):
-	with tf.Graph().as_default(), tf.Session() as sess:
-
-		img_format = path.lower()[-3:]
-		img_bytes = tf.read_file(path)
-
-		if img_format == 'png':
-			image = tf.image.decode_png(img_bytes, channels=3)
-		else:
-			image = tf.image.decode_jpeg(img_bytes, channels=3)
-
-		if height is not None and width is not None:
-			image = tf.image.resize_images(image, [height, width])
-
-		image = tf.stack([image])
-
-		return image.eval()
+from os import listdir
+from os.path import join, splitext
 
 
-def get_batch_images(paths, height=None, width=None):
+def list_images(directory):
+	images = []
+	for file in listdir(directory):
+		name = file.lower()
+		if name.endswith('.png'):
+			images.append(join(directory, file))
+		elif name.endswith('.jpg'):
+			images.append(join(directory, file))
+		elif name.endswith('.jpeg'):
+			images.append(join(directory, file))
+	return images
+
+
+def get_images(paths, height=None, width=None):
+	if isinstance(paths, str):
+		paths = [paths]
+
 	with tf.Graph().as_default(), tf.Session() as sess:
 
 		images = []
 		for path in paths:
-			img_format = path.lower()[-3:]
+			is_png = path.lower().endswith('png')
 			img_bytes = tf.read_file(path)
 
-			if img_format == 'png':
+			if is_png:
 				image = tf.image.decode_png(img_bytes, channels=3)
 			else:
 				image = tf.image.decode_jpeg(img_bytes, channels=3)
+
+			if height is not None and width is not None:
+				image = tf.image.resize_images(image, [height, width])
 			
 			images.append(image)
 
 		images = tf.stack(images)
 
-		if height is not None and width is not None:
-			images = tf.image.resize_images(images, [height, width])
-
 		return images.eval()
+
+
+def save_images(paths, datas, postfix='-stylized'):
+	if isinstance(paths, str):
+		paths = [paths]
+
+	assert(len(paths) == len(datas))
+
+	with tf.Graph().as_default(), tf.Session() as sess:
+
+		for i, path in enumerate(paths):
+			data = datas[i]
+			is_png = path.lower().endswith('png')
+
+			if is_png:
+				image = tf.image.encode_png(data)
+			else:
+				image = tf.image.encode_jpeg(data)
+
+			if postfix is not None:
+				name, ext = splitext(path)
+				path = name + postfix + ext
+
+			tf.write_file(path, image).run()
 
