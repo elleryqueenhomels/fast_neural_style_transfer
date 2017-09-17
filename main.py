d639298a5b9bf32171d10e8a5e9bd1d64b150f86
Wengao Ye <elleryqueenhomels@users.noreply.github.com>
2017-09-18 00:55:13 +0800

    A Test Demo

--- main.py
+++ main.py
@@ -6,46 +6,31 @@ from generate import generate
 from utils import list_images
 
 
-is_training = True
-condition = 1
+STYLE = 'starry'
+IS_TRAINING = False
+
 
 if __name__ == '__main__':
     content_weight = 1.0
-    style_weight = 8.0
-    tv_weight = 0.01
+    style_weight   = 8.0
+    tv_weight      = 0.01
 
-    style_target = 'images/style/wave.jpg'
-    model_save_path = 'models/wave.ckpt-done'
+    style_target = 'images/style/' + STYLE + '.jpg'
+    model_save_path = 'models/' + STYLE + '.ckpt-done'
 
-    if is_training:
+    if IS_TRAINING:
         content_targets = list_images('../MS_COCO')
 
         train(content_targets, style_target, content_weight, style_weight, tv_weight, save_path=model_save_path, debug=True)
+
         print('\nSuccessfully! Done training...\n')
     else:
         model_path = model_save_path
         output_save_path = 'outputs'
 
-        if condition == 1:
-            content_targets = list_images('images/content')
-            generated_images = generate(content_targets, model_path, save_path=output_save_path, postfix='-wave')
-        elif condition == 2:
-            content_targets = list_images('images/content')
-            generated_images = generate(content_targets, model_path, save_path=output_save_path, postfix='-wave')
-        elif condition == 3:
-            content_targets = ['images/content/lena.png', 'images/content/lena2.png', 'images/content/lena3.png', 'images/content/lena4.png']
-            generated_images = generate(content_targets, model_path, is_same_size=True, save_path=output_save_path, postfix='-wave')
-        else:
-            content_targets = 'images/content/scream.jpg'
-            generated_images = generate(content_targets, model_path, save_path=output_save_path, postfix='-wave')
+        content_targets = list_images('images/content')
+        generated_images = generate(content_targets, model_path, save_path=output_save_path, postfix='-' + STYLE)
 
         print('\ntype(generated_images):', type(generated_images))
         print('\nlen(generated_images):', len(generated_images), '\n')
-        
-
-        # import matplotlib.pyplot as plt
-
-        # for img in generated_images:
-        #     plt.imshow(img)
-        #     plt.show()
 
