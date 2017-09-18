c8bc13640eb1a758bc3fdd803d8dccb9bcd35b5f
Wengao Ye <elleryqueenhomels@users.noreply.github.com>
2017-09-18 16:46:43 +0800

    A Test Demo

--- main.py
+++ main.py
@@ -1,36 +1,50 @@
 # Demo - train the style transfer network & use it to generate an image
 
 
+from __future__ import print_function
+
 from train import train
 from generate import generate
 from utils import list_images
 
 
-STYLE = 'wave'
 IS_TRAINING = True
+
+STYLES = ['wave', 'udnie', 'starry', 'scream', 'denoised_starry', 'mosaic']
+
+CONTENT_WEIGHTS = [ 1.0,   1.0,  1.0,  1.0,  1.0,   1.0]
+STYLE_WEIGHTS   = [10.0, 100.0, 15.0, 15.0, 15.0, 100.0]
+TV_WEIGHTS      = [0.01,  0.01, 0.01, 0.01, 0.01,  0.01]
 
 
 if __name__ == '__main__':
-    content_weight = 1.0
-    style_weight   = 8.0
-    tv_weight      = 0.01
-
-    style_target = 'images/style/' + STYLE + '.jpg'
-    model_save_path = 'models/' + STYLE + '.ckpt-done'
 
     if IS_TRAINING:
-        content_targets = list_images('../MS_COCO')
 
-        train(content_targets, style_target, content_weight, style_weight, tv_weight, save_path=model_save_path, debug=True)
+        content_targets = list_images('../MS_COCO') # path to training dataset
 
-        print('\nSuccessfully! Done training...\n')
+        for style, content_weight, style_weight, tv_weight in zip(STYLES, CONTENT_WEIGHTS, STYLE_WEIGHTS, TV_WEIGHTS):
+
+            print('\n\nBegin to train the network with the style "%s"...\n' % style)
+
+            style_target = 'images/style/' + style + '.jpg'
+            model_save_path = 'models/' + style + '.ckpt-done'
+
+            train(content_targets, style_target, content_weight, style_weight, tv_weight, save_path=model_save_path, debug=True)
+
+            print('\nSuccessfully! Done training...\n')
     else:
-        model_path = model_save_path
-        output_save_path = 'outputs'
 
-        content_targets = list_images('images/content')
-        generated_images = generate(content_targets, model_path, save_path=output_save_path, postfix='-' + STYLE)
+        for style in STYLES:
 
-        print('\ntype(generated_images):', type(generated_images))
-        print('\nlen(generated_images):', len(generated_images), '\n')
+            print('\n\nBegin to generate pictures with the style "%s"...\n' % style)
+
+            model_path = 'models/' + style + '.ckpt-done'
+            output_save_path = 'outputs'
+
+            content_targets = list_images('images/content')
+            generated_images = generate(content_targets, model_path, save_path=output_save_path, postfix='-' + style)
+
+            print('\ntype(generated_images):', type(generated_images))
+            print('\nlen(generated_images):', len(generated_images), '\n')
 
