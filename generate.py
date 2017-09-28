a8710f84389905f6bf3fdd6853b7c2c513c5aa0d
Wengao Ye <elleryqueenhomels@users.noreply.github.com>
2017-09-28 10:40:47 -0500

    Generate Images with Style Transferring

--- generate.py
+++ generate.py
@@ -1,26 +1,26 @@
 # Use a trained Image Transform Net to generate
 # a style transferred image with a specific style
 
-
 import tensorflow as tf
 import image_transform_net as itn
 
 from utils import get_images, save_images
 
 
-def generate(contents_path, model_path, is_same_size=False, resize_height=None, resize_width=None, save_path=None, postfix='-stylized'):
+def generate(contents_path, model_path, is_same_size=False, resize_height=None, resize_width=None, save_path=None, prefix='stylized-', suffix=None):
     if isinstance(contents_path, str):
         contents_path = [contents_path]
 
     if is_same_size or (resize_height is not None and resize_width is not None):
-        outputs = _handler1(contents_path, model_path, resize_height=resize_height, resize_width=resize_width, save_path=save_path, postfix=postfix)
+        outputs = _handler1(contents_path, model_path, resize_height=resize_height, resize_width=resize_width, 
+            save_path=save_path, prefix=prefix, suffix=suffix)
         return list(outputs)
     else:
-        outputs = _handler2(contents_path, model_path, save_path=save_path, postfix=postfix)
+        outputs = _handler2(contents_path, model_path, save_path=save_path, prefix=prefix, suffix=suffix)
         return outputs
 
 
-def _handler1(content_path, model_path, resize_height=None, resize_width=None, save_path=None, postfix='-stylized'):
+def _handler1(content_path, model_path, resize_height=None, resize_width=None, save_path=None, prefix=None, suffix=None):
     # get the actual image data, output shape: (num_images, height, width, color_channels)
     content_target = get_images(content_path, resize_height, resize_width)
 
@@ -37,12 +37,12 @@ def _handler1(content_path, model_path, resize_height=None, resize_width=None, s
         output = sess.run(output_image, feed_dict={content_image: content_target})
 
     if save_path is not None:
-        save_images(content_path, output, save_path, postfix)
+        save_images(content_path, output, save_path, prefix=prefix, suffix=suffix)
 
     return output
 
 
-def _handler2(content_path, model_path, save_path=None, postfix='-stylized'):
+def _handler2(content_path, model_path, save_path=None, prefix=None, suffix=None):
     with tf.Graph().as_default(), tf.Session() as sess:
         # build the dataflow graph
         content_image = tf.placeholder(tf.float32, shape=(1, None, None, 3), name='content_image')
@@ -60,7 +60,7 @@ def _handler2(content_path, model_path, save_path=None, postfix='-stylized'):
             output.append(result[0])
 
     if save_path is not None:
-        save_images(content_path, output, save_path, postfix)
+        save_images(content_path, output, save_path, prefix=prefix, suffix=suffix)
 
     return output
 
